e7aaf9819d6e41e9524fd378ff193e88aa2444d1
sasca37 <sasca37@naver.com>
2021-07-13 02:36:01 +0900

    0

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    // 할인과 관련된 정보를 discountpolicy 에 두었으므로 오더 서비스 입장에선 단일책임원칙을 잘 설계한 것이된다.
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
