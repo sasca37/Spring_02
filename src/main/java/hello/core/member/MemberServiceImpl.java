d1e41049f32d9dd988190d4ec0e9427274f7c5d9
sasca37 <sasca37@naver.com>
2021-07-13 03:15:31 +0900

    0

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    //final 객체 자체를 다른 객체로 변경할 수 없으나 객체 필드는 변경할 수 있다 .
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository{
-
+    // 모든 객체에서 공유하기 위한 store를 static으로 선언 ( 선언 안할 시 오류 발생)
     private static Map<Long, Member> store = new HashMap<>();
 
     @Override
